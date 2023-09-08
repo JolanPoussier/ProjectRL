620439e76a82c512dde161a1667546e4bd854cdf
JolanPoussier <jojopoussier@gmail.com>
2023-09-08 11:44:35 +0200

    feat: input v1 done

--- src/components/sidebar/searchBar/searchBar.tsx
+++ src/components/sidebar/searchBar/searchBar.tsx
@@ -10,33 +10,40 @@ import SuggestionSide from "./suggestionSide";
 export default function SearchBar ({displayFunc}: {displayFunc?: Function}) {
 
     const [inputValue, setInputValue] = useState<any>("");
+    const [overlay, setOverlay] = useState(false);
     const router = useRouter();
 
     const handleInputChange = (e: any) => {
         setInputValue(e.target.value);
+        (e.target.value === "") ? setOverlay(false) : "";
     }
-    const handleSubmit = (e:any) => {
+    const handleSubmit = (e: any) => {
         e.preventDefault();
         router.push(`/${inputValue}`);    
+        displayFunc ? displayFunc() : "";
     }
 
     return (
-        <div className={style.inputContainer}>
-            <form onSubmit={handleSubmit}>
-                <div className={inputValue ? style.inputDivBorder : style.inputDiv}>
-                    <input 
-                        className={style.input}
-                        placeholder="Trouves ta méchanique" 
-                        type="text" 
-                        value={inputValue} 
-                        onChange={handleInputChange} 
-                    />
-                    <button onClick={handleSubmit} className={style.searchIcon}>
-                        <Image alt="searchIcon" src={searchIcon} width={30} height={30}/>
-                    </button>
-                </div>
-            </form>
-            {inputValue && <SuggestionSide inputValue={inputValue} resetInput={setInputValue} displayFunc={displayFunc}/>}
-        </div>
+        <>
+            <div className={ overlay ? style.overlaySearchBar_active : style.overlaySearchBar } onClick={() => setOverlay(false)}></div>
+            <div className={style.inputContainer}>
+                <form onSubmit={handleSubmit}>
+                    <div className={inputValue && overlay ? style.inputDivBorder : style.inputDiv}>
+                        <input 
+                            className={style.input}
+                            placeholder="Trouves ta méchanique" 
+                            type="text" 
+                            value={inputValue} 
+                            onChange={handleInputChange}
+                            onClick={() => setOverlay(true)} 
+                        />
+                        <button onClick={handleSubmit} className={style.searchIcon}>
+                            <Image alt="searchIcon" src={searchIcon} width={30} height={30}/>
+                        </button>
+                    </div>
+                </form>
+                {inputValue && overlay && <SuggestionSide inputValue={inputValue} resetInput={setInputValue} displayFunc={displayFunc} setOverlay={setOverlay}/>}
+            </div>
+        </>
     )
 }
