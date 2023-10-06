210aedb5e9ea23b7351c2327880e722e4afef73c
JolanPoussier <jojopoussier@gmail.com>
2023-10-06 16:11:06 +0200

    feat: controllerModal add

--- src/components/moveCard/controllerModal.tsx
+++ src/components/moveCard/controllerModal.tsx
@@ -13,19 +13,28 @@ export default function ControllerModal() {
   return (
     <>
       <div
-        className={displayModal ? style.controllerModalOn : style.controllerModal}
+        className={displayModal ? style.controllerOverlayOn : style.controllerOverlay}
         onClick={() => setDisplayModal(false)}
       >
         <Image className={style.modalCloseButton} src={closeButton} alt="close button" width={40} height={40} />
-        <Image className={style.modalRotateIcon} src={rotationIcon} alt="rotation icon" width={60} height={60} />
+        <Image
+          onClick={() => setDisplayModal(false)}
+          className={style.modalRotateIcon}
+          src={rotationIcon}
+          alt="rotation icon"
+          width={60}
+          height={60}
+        />
       </div>
-      <Image
-        className={displayModal ? style.controllerDisplayOn : style.controllerDisplay}
-        src={controllerLarge}
-        alt="Controller with disposition of abilities"
-        width={700}
-        height={300}
-      />
+      <div className={displayModal ? style.controllerDisplayOn : style.controllerDisplay}>
+        <Image
+          className={style.controllerImage}
+          src={controllerLarge}
+          alt="Controller with disposition of abilities"
+          width={0}
+          height={0}
+        />
+      </div>
       <button className={style.controllerButton} onClick={() => setDisplayModal(true)}>
         <Image src={controller} alt="controller icon" width={70} height={50} />
       </button>
