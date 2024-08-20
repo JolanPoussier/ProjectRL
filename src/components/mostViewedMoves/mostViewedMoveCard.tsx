075815b4fd3a411d65444f8035e62b15cc60c9f0
JolanPoussier <jojopoussier@gmail.com>
2024-08-20 14:46:31 +0200

    improves images quality

--- src/components/mostViewedMoves/mostViewedMoveCard.tsx
+++ src/components/mostViewedMoves/mostViewedMoveCard.tsx
@@ -25,8 +25,8 @@ export default function MostViewedMoveCard({
           className={style.image}
           alt={`Image du move ${title}`}
           src={`/img/thumbnails/${thumbnail}_th.png`}
-          width={950}
-          height={950}
+          width={550}
+          height={550}
         />
         <div className={style.header}>
           <h3 className={style.titleCard}>{title}</h3>
